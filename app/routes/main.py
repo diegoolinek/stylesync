98bf31d51cbc4ed06019a629d2c4a1d1a201603c
Diego Olinek <164029535+diegoolinek@users.noreply.github.com>
2025-10-20 13:51:23 -0300

    Refactor product routes to include update functionality and improve response formatting

--- app/routes/main.py
+++ app/routes/main.py
@@ -3,7 +3,7 @@ from app.models.user import LoginPayload
 from pydantic import ValidationError
 from app import db
 from bson import ObjectId
-from app.models.products import Product, ProductDBModel
+from app.models.products import Product, ProductDBModel, UpdateProduct
 from app.decorators import token_required
 from datetime import datetime, timedelta, timezone
 import jwt
@@ -67,34 +67,52 @@ def get_product_by_id(product_id):
     try:
         oid = ObjectId(product_id)
     except Exception as e:
-        return jsonify(message=f"Erro ao transformar o {product_id} em ObjectId: {e}")
+        return jsonify({"message":f"Erro ao transformar o {product_id} em ObjectId: {e}"})
     
     product = db.products.find_one({'_id': oid})
     if product:
         product_model = ProductDBModel(**product).model_dump(by_alias=True, exclude_none=True)
         return jsonify(product_model)
     else:
-        return jsonify(message=f"Produto com o ID {product_id} não encontrado"), 404
+        return jsonify({"message":f"Produto com o ID {product_id} não encontrado"}), 404
 
 
 # RF: o sistema deve permitir que o usuário atualize as informações de um produto
-@main_bp.route('/product/<int:product_id>', methods=['PUT'])
-def update_product(product_id):
-    return jsonify(message=f"Esta é a rota de atualização do produto {product_id}")
+@main_bp.route('/product/<string:product_id>', methods=['PUT'])
+@token_required
+def update_product(token, product_id):
+    try:
+        oid = ObjectId(product_id)
+        update_data = UpdateProduct(**request.get_json())
+        
+    except ValidationError as e:
+        return jsonify({"error":e.errors()}), 400
+    
+    update_result = db.products.update_one(
+            {'_id': oid}, 
+            {'$set': update_data.model_dump(exclude_unset=True)}
+        )
+    
+    if update_result.matched_count == 0:
+        return jsonify({"error":f"Produto com o ID {product_id} não encontrado"}), 404
+
+    updated_product = db.products.find_one({"_id": oid})
+
+    return jsonify(ProductDBModel(**updated_product).model_dump(by_alias=True, exclude_none=True))
 
 
 # RF: o sistema deve permitir que o usuário delete um produto
-@main_bp.route('/product/<int:product_id>', methods=['DELETE'])
+@main_bp.route('/product/<string:product_id>', methods=['DELETE'])
 def delete_product(product_id):
-    return jsonify(message=f"Esta é a rota de deleção do produto {product_id}")
+    return jsonify({"message":f"Esta é a rota de deleção do produto {product_id}"})
 
 
 # RF: o sistema deve permitir que o usuário delete um produto
 @main_bp.route('/sales/upload', methods=['POST'])
 def upload_sales():
-    return jsonify(message="Esta é a rota de upload de vendas")
+    return jsonify({"message":"Esta é a rota de upload de vendas"})
 
 
 @main_bp.route('/')
 def index():
-    return jsonify(message="Bem vindo ao StyleSync!")
+    return jsonify({"message":"Bem vindo ao StyleSync!"})
